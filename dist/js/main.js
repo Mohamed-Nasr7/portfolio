153ea7433f8d4581a6a57145b93b602e0cc90a89
mohamed <mohammed.nasr393@gmail.com>
2020-07-17 18:10:37 +0200

    parallax header

--- dist/js/main.js
+++ dist/js/main.js
@@ -86,3 +86,31 @@ window.addEventListener('scroll', function(){
 arrow.addEventListener('click', function(){
     window.scrollTo({top: 0, behavior: 'smooth'});
 })
+
+
+
+/* function parallax(element, distance, speed){
+    var item = document.querySelector(element);
+    item.style.transform = "translateY("+ distance * speed +"px)";
+} */
+
+var header = document.querySelector('.header');
+
+
+window.addEventListener('scroll', function(){
+
+    var name = document.querySelector('header h1');
+    var job = document.querySelector('header h6');
+    var pos = (window.pageYOffset / 3) + "px";
+
+    header.style.backgroundPosition = "center " + pos;
+    name.style.transform = "translate(-50%, "+ ((window.scrollY * -0.2) - 50) +"%)";
+    job.style.transform = "translate(-50%, "+ ((window.scrollY * 1.2) - 50) +"%)";
+
+    if(window.scrollY == 0){
+        name.style.textShadow = "none";
+    }else{
+        name.style.textShadow = "0 1px "+ window.scrollY * 0.05 +"px #fff, 0 1px "+ window.scrollY * 0.08 +"px #fff";
+    }
+    job.style.opacity = 1 - (window.scrollY / 300);
+});
